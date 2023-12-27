81a073c6f62db2d6e425945680d6bfdaafdb5fb7
Diagmerc <91744556+Diagmerc@users.noreply.github.com>
2023-12-27 10:06:22 +0300

    create entities

---
+++ src/main/java/ru/buyitcookit/buyitcookit/entity/Product.java
@@ -0,0 +1,28 @@
+package ru.buyitcookit.buyitcookit.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "products")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Integer weight;
+
+    private Integer price;
+
+    private Integer quantum;
+}
